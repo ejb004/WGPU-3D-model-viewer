a2e20148d076c55ff8f0af873abc3dc06932dcf9
Ehan <ethan@subblue.com>
2023-12-27 15:35:23 +0000

    Camera adjust

---
+++ src/utils.rs
@@ -0,0 +1,31 @@
+#[repr(C)] // make 'C-like' memory storage for compatibility with glsl and gpu
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+pub const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [-0.0868241, 0.49240386, 0.1],
+        color: [0.5, 0.0, 0.5],
+    }, // A
+    Vertex {
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // B
+    Vertex {
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // C
+    Vertex {
+        position: [0.35966998, -0.3473291, 1.0],
+        color: [0.5, 0.0, 0.5],
+    }, // D
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // E
+];
+
+pub const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
